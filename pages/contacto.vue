7c64997d1a9502049de1d5157d5875717db9b39c
Manana101 <manana.jaworska@gmail.com>
2019-07-15 00:08:34 +0200

    try embedding a typeform

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -1,42 +1,10 @@
 <template>
   <v-container>
 
-    <v-layout align-center mb-4 class="jumbotron">
-      <v-flex>
-        <h1 class="display-2 font-weight-bold">{{ h1 }}</h1>
-      </v-flex>
-    </v-layout>
-    <v-layout justify-center>
+    <v-layout justify-center class="typeform">
       <v-flex xs12 lg8>
-        <v-img :src="image" aspect-ratio="1.7" />
-        <p class="mt-4 pt-3">
-          {{ data.address_line1 }}<br/>
-          {{ data.address_line2 }}
-        </p>
-        <p class="mb-4 pb-2">
-          {{ data.webpage }}<br/>
-          {{ data.mail }}<br/>
-          {{ data.tel }}
-        </p>
-        <v-layout justify-center>
-          <v-btn large depressed light href="mailto:olga@traductordepolaco.eu">Envía un correo</v-btn>
-          <v-btn large outline href="tel:+34693592793">Llama</v-btn>
-        </v-layout>
+        <div class="typeform-widget" data-url="https://manana307418.typeform.com/to/zqdTJK" data-hide-headers=true data-hide-footer=true style="width: 100%; height: 500px;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script>
       </v-flex>
-    </v-layout>
-
-    <v-layout fluid justify-center>
-      <iframe
-        class="map"
-        width="100%"
-        height="450px"
-        id="gmap_canvas"
-        src="https://maps.google.com/maps?q=calle%20Islas%20Baleares%202%2046388%20Godelleta%2C%20Valencia&t=&z=13&ie=UTF8&iwloc=&output=embed"
-        frameborder="0"
-        scrolling="no"
-        marginheight="0"
-        marginwidth="0"
-      ></iframe>
     </v-layout>
 
     <v-layout justify-center wrap>
@@ -62,6 +30,25 @@
           <em>{{ data.horarios_line2 }}</em>
         </p>
       </v-flex>
+    </v-layout>
+
+    <v-layout fluid justify-center>
+      <iframe
+        class="map"
+        width="100%"
+        height="450px"
+        id="gmap_canvas"
+        src="https://maps.google.com/maps?q=calle%20Islas%20Baleares%202%2046388%20Godelleta%2C%20Valencia&t=&z=13&ie=UTF8&iwloc=&output=embed"
+        frameborder="0"
+        scrolling="no"
+        marginheight="0"
+        marginwidth="0"
+      ></iframe>
+    </v-layout>
+
+    <v-layout justify-center class="buttons">
+      <v-btn large depressed light href="mailto:olga@traductordepolaco.eu">Envía un correo</v-btn>
+      <v-btn large outline href="tel:+34693592793">Llama</v-btn>
     </v-layout>
 
   </v-container>
@@ -100,11 +87,16 @@ export default {
     font-size: 18px;
   }
 
-  .jumbotron {
-    height: 120px;
+  .map {
+    margin: 60px 0;
   }
 
-  .map {
-    margin: 100px 0;
+  .typeform {
+    margin-top: 20px;
+    margin-bottom: 80px;
+  }
+
+  .buttons {
+    margin-bottom: 40px;
   }
 </style>
